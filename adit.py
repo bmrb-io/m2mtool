55b0555860916a5d0469e053df4db95c84bac022
Jon Wedell <wedell@bmrb.wisc.edu>
2017-07-28 14:50:38 -0500

    Remove HTTPParser class, just parse directly for now.

--- adit.py
+++ adit.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python3
 
 import re
+import logging
 import webbrowser
 from html.parser import HTMLParser
 
 import requests
 
-class ADITSession(HTMLParser):
+# Set up logging
+logging.basicConfig()
 
-    def handle_starttag(self, tag, attributes):
-        for name, value in attributes:
-            if name == 'href' and tag == "a":
-                self.url = value
+def get_session_id(text):
+    """ Returns the session ID."""
 
-    def get_session_id(self):
-        """ Return the session ID."""
+    matches = re.search("ufid=(.+?)&", request.text)
+    if matches and len(matches.groups()) > 0:
+        return matches.groups(0)[0]
+    else:
+        raise ValueError("No session.")
 
-        matches = re.search("ufid=(.+?)&", self.url)
-        if matches and len(matches.groups()) > 0:
-            return matches.groups(0)[0]
-        else:
-            raise ValueError("No session.")
+def get_activate_url(text):
+    """ Returns the session ID."""
 
-    def get_url(self):
-        return self.url
+    matches = re.search("URL=(.+?)\"", request.text)
+    if matches and len(matches.groups()) > 0:
+        return matches.groups(0)[0]
+    else:
+        raise ValueError("No session.")
 
-# Start doing the session
-session = requests.Session()
-request = session.post("http://144.92.167.205/cgi-bin/bmrb-adit/adit-session-driver")
+# Start the ADIT session
+with requests.Session() as session:
+    logging.info("Creating session.")
+    request = session.post("http://144.92.167.205/cgi-bin/bmrb-adit/adit-session-driver")
 
+    # Find the session ID
+    sid = get_session_id(request.text)
+    logging.info("Session ID: %s" % sid)
 
-adit = ADITSession()
-adit.feed(request.text)
-print(test)
+    # Activate the session (don't know why this is needed...)
+    logging.info("Activating session.")
+    request = session.get(get_activate_url(request.text))
+
+print(sid)
