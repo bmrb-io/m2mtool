63f7e1f7fc8a2ed7e35488dc56cd8b2e29382b25
Jon Wedell <jonwedell@gmail.com>
2019-01-07 10:59:41 -0600

    Update run script to work regardless of where called from.

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-if [[ ! -d "./env" ]]; then
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [[ ! -d "${DIR}/env" ]]; then
+  cd ${DIR}
   echo "Performing initial install..."
   pip3 install virtualenv > /dev/null
   python3 -m virtualenv env > /dev/null
   source ./env/bin/activate
   pip3 install -r requirements.txt > /dev/null
 fi
-source ./env/bin/activate
 ./m2mtool.py "$@"
