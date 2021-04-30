087cf78fd0dfec98eef58b26ad9d49c4385e8401
jchin <jochin@uchc.edu>
2021-04-30 16:45:07 -0400

    add code to create list of selected files to be passed to api

--- file_selector/window.py
+++ file_selector/window.py
@@ -12,6 +12,12 @@ class Window(QtWidgets.QWidget):
         ui_path = os.path.join(os.path.dirname(__file__), 'form.ui')
         uic.loadUi(ui_path, self)
 
+        # TODO: add code to assign self.directory
+        test_directory = "/home/nmrbox/0015/jchin/Qt5.12.10/"
+
+        self.directory = test_directory
+        self.selected_files = []
+
         # center window on screen
         qtRectangle = self.frameGeometry()
         centerPoint = QDesktopWidget().availableGeometry().center()
@@ -19,52 +25,80 @@ class Window(QtWidgets.QWidget):
         self.move(qtRectangle.topLeft())
 
         # populate list with files and subfolders from directory
-        test_directory = "/home/nmrbox/0015/jchin/Qt5.12.10/"
-        self.populate_files(test_directory)
+        self.populate_files()
 
         # connect push buttons to methods
         self.pushButton_submit.clicked.connect(self.submit)
         self.pushButton_cancel.clicked.connect(self.cancel)
 
-    def populate_files(self, directory):
-        subfolders = [s for s in os.listdir(directory) if os.path.isdir(os.path.join(directory, s))]
-        files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    def populate_files(self):
+        # search self.directory and create separate lists for subdirectories and files
+        subdirectories = [s for s in os.listdir(self.directory) if os.path.isdir(os.path.join(self.directory, s))]
+        files = [f for f in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, f))]
 
-        for subfolder in subfolders:
+        # add subdirectories to listWidget
+        for subdirectory in subdirectories:
             item = QtWidgets.QListWidgetItem()
-            item.setText(subfolder)
+            item.setText(subdirectory)
             item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
             item.setCheckState(QtCore.Qt.Checked)
-            icon = QIcon(QApplication.style().standardIcon(QStyle.SP_DirIcon))
-            item.setIcon(icon)
+            item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_DirIcon)))
+            item.setData(QtCore.Qt.UserRole, "subdirectory")
             self.listWidget_files.addItem(item)
 
+        # add files to listWidget
         for file in files:
             item = QtWidgets.QListWidgetItem()
             item.setText(file)
             item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
             item.setCheckState(QtCore.Qt.Checked)
-            icon = QIcon(QApplication.style().standardIcon(QStyle.SP_FileIcon))
-            item.setIcon(icon)
+            item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_FileIcon)))
+            item.setData(QtCore.Qt.UserRole, "file")
             self.listWidget_files.addItem(item)
 
-        # files = [f for f in os.listdir(directory) if os.path.isdir(os.path.join(directory, f)) or
-        # os.path.isfile(os.path.join(directory, f))]
+        # files_and_subdirectories = [f for f in os.listdir(self.directory) if
+        #                         os.path.isdir(os.path.join(self.directory, f)) or
+        #                         os.path.isfile(os.path.join(self.directory, f))]
         #
-        # for file in files:
+        # for each in files_and_subdirectories:
         #     item = QtWidgets.QListWidgetItem()
-        #     item.setText(file)
+        #     item.setText(each)
         #     item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
         #     item.setCheckState(QtCore.Qt.Checked)
-        #     if os.path.isfile(os.path.join(directory, file)):
-        #         icon = QIcon(QApplication.style().standardIcon(QStyle.SP_FileIcon))
-        #     elif os.path.isdir(os.path.join(directory, file)):
-        #         icon = QIcon(QApplication.style().standardIcon(QStyle.SP_DirIcon))
-        #     item.setIcon(icon)
+        #     if os.path.isfile(os.path.join(self.directory, each)):
+        #         item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_FileIcon)))
+        #         item.setData(QtCore.Qt.UserRole, "file")
+        #     elif os.path.isdir(os.path.join(self.directory, each)):
+        #         item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_DirIcon)))
+        #         item.setData(QtCore.Qt.UserRole, "subdirectory")
         #     self.listWidget_files.addItem(item)
 
     def submit(self):
-        pass
+        selected_subdirectories = []
+
+        # add selected files to selected_files list and make list of selected subdirectories
+        for index in range(self.listWidget_files.count()):
+            if self.listWidget_files.item(index).checkState() == QtCore.Qt.Checked:
+                if self.listWidget_files.item(index).data(QtCore.Qt.UserRole) == "file":
+                    self.selected_files.append(self.listWidget_files.item(index).text())
+                elif self.listWidget_files.item(index).data(QtCore.Qt.UserRole) == "subdirectory":
+                    selected_subdirectories.append(self.listWidget_files.item(index).text())
+
+        # recursively add files from selected subdirectories to selected_files list
+        for subdir in selected_subdirectories:
+            self.add_subdirectory_files(subdir)
+
+        # retrieve deposition nickname
+        nickname = self.plainTextEdit_nickname.toPlainText()
+
+        # TODO: pass self.selected_files, nickname to api
+
+    def add_subdirectory_files(self, subdirectory):
+        for each in os.listdir(os.path.join(os.path.dirname(self.directory), subdirectory)):
+            if os.path.isfile(os.path.join(os.path.dirname(self.directory), subdirectory, each)):
+                self.selected_files.append(f'{subdirectory}/{each}')
+            elif os.path.isdir(os.path.join(os.path.dirname(self.directory), subdirectory, each)):
+                self.add_subdirectory_files(f'{subdirectory}/{each}')
 
     def cancel(self):
         self.close()
