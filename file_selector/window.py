9c8846f68579fbc489886dc34e1e560a434d1a1d
jchin <jochin@uchc.edu>
2021-05-04 17:21:00 -0400

    edit populate_files method to sort directory alphabetically and by item type (file v. folder)

--- file_selector/window.py
+++ file_selector/window.py
@@ -1,8 +1,12 @@
 import os
+import logging
 
 from PyQt5 import QtWidgets, QtCore, uic
 from PyQt5.QtWidgets import QStyle, QApplication, QDesktopWidget
 from PyQt5.QtGui import QIcon
+
+logging.basicConfig(filename="debug.log", format='%(asctime)s %(message)s')
+logging.getLogger().setLevel(logging.DEBUG)
 
 
 class Window(QtWidgets.QWidget):
@@ -19,12 +23,12 @@ class Window(QtWidgets.QWidget):
         self.selected_files = []
 
         # center window on screen
-        qtRectangle = self.frameGeometry()
-        centerPoint = QDesktopWidget().availableGeometry().center()
-        qtRectangle.moveCenter(centerPoint)
-        self.move(qtRectangle.topLeft())
+        qt_rectangle = self.frameGeometry()
+        center_point = QDesktopWidget().availableGeometry().center()
+        qt_rectangle.moveCenter(center_point)
+        self.move(qt_rectangle.topLeft())
 
-        # populate list with files and subfolders from directory
+        # populate list with files and subdirectories from directory
         self.populate_files()
 
         # connect push buttons to methods
@@ -32,51 +36,33 @@ class Window(QtWidgets.QWidget):
         self.pushButton_cancel.clicked.connect(self.cancel)
 
     def populate_files(self):
-        # search self.directory and create separate lists for subdirectories and files
-        subdirectories = [s for s in os.listdir(self.directory) if os.path.isdir(os.path.join(self.directory, s))]
-        files = [f for f in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, f))]
 
-        # add subdirectories to listWidget
-        for subdirectory in subdirectories:
-            item = QtWidgets.QListWidgetItem()
-            item.setText(subdirectory)
-            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
-            item.setCheckState(QtCore.Qt.Checked)
-            item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_DirIcon)))
-            item.setData(QtCore.Qt.UserRole, "subdirectory")
-            self.listWidget_files.addItem(item)
+        def alpha_and_folder(item):
+            return os.path.isfile(os.path.join(self.directory, item)), item.lower()
 
-        # add files to listWidget
-        for file in files:
-            item = QtWidgets.QListWidgetItem()
-            item.setText(file)
-            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
-            item.setCheckState(QtCore.Qt.Checked)
-            item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_FileIcon)))
-            item.setData(QtCore.Qt.UserRole, "file")
-            self.listWidget_files.addItem(item)
+        # sort the files/subdirectories alphabetically and by item type (file v. folder)
+        sorted_directory = sorted(os.listdir(self.directory), key=alpha_and_folder)
 
-        # files_and_subdirectories = [f for f in os.listdir(self.directory) if
-        #                         os.path.isdir(os.path.join(self.directory, f)) or
-        #                         os.path.isfile(os.path.join(self.directory, f))]
-        #
-        # for each in files_and_subdirectories:
-        #     item = QtWidgets.QListWidgetItem()
-        #     item.setText(each)
-        #     item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
-        #     item.setCheckState(QtCore.Qt.Checked)
-        #     if os.path.isfile(os.path.join(self.directory, each)):
-        #         item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_FileIcon)))
-        #         item.setData(QtCore.Qt.UserRole, "file")
-        #     elif os.path.isdir(os.path.join(self.directory, each)):
-        #         item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_DirIcon)))
-        #         item.setData(QtCore.Qt.UserRole, "subdirectory")
-        #     self.listWidget_files.addItem(item)
+        # add each file and subdirectory to the list widget
+        for each in sorted_directory:
+            list_item = QtWidgets.QListWidgetItem()
+            if os.path.isfile(os.path.join(self.directory, each)):
+                list_item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_FileIcon)))
+                list_item.setData(QtCore.Qt.UserRole, "file")
+            elif os.path.isdir(os.path.join(self.directory, each)):
+                list_item.setIcon(QIcon(QApplication.style().standardIcon(QStyle.SP_DirIcon)))
+                list_item.setData(QtCore.Qt.UserRole, "subdirectory")
+            else:
+                continue
+            list_item.setText(each)
+            list_item.setFlags(list_item.flags() | QtCore.Qt.ItemIsUserCheckable)
+            list_item.setCheckState(QtCore.Qt.Checked)
+            self.listWidget_files.addItem(list_item)
 
     def submit(self):
         selected_subdirectories = []
 
-        # add selected files to selected_files list and make list of selected subdirectories
+        # add selected files to self.selected_files and create list of selected subdirectories
         for index in range(self.listWidget_files.count()):
             if self.listWidget_files.item(index).checkState() == QtCore.Qt.Checked:
                 if self.listWidget_files.item(index).data(QtCore.Qt.UserRole) == "file":
@@ -92,8 +78,12 @@ class Window(QtWidgets.QWidget):
         nickname = self.plainTextEdit_nickname.toPlainText()
 
         # TODO: pass self.selected_files, nickname to api
+        logging.debug("Nickname: %s, Selected Files: %s", nickname, self.selected_files)
+
+        self.close()
 
     def add_subdirectory_files(self, subdirectory):
+        # adds files from subdirectory to self.selected_files
         for each in os.listdir(os.path.join(os.path.dirname(self.directory), subdirectory)):
             if os.path.isfile(os.path.join(os.path.dirname(self.directory), subdirectory, each)):
                 self.selected_files.append(f'{subdirectory}/{each}')
