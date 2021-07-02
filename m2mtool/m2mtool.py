06b49008a9dbb84a628e15b3ecff67c6c96acb3a
jchin <jochin@uchc.edu>
2021-07-02 08:39:05 -0400

    remove commented text (obsolete after refactoring) from m2mtool.py

--- m2mtool/m2mtool.py
+++ m2mtool/m2mtool.py
@@ -26,35 +26,13 @@ import json
 import logging
 import os
 import sys
-# import time
 import webbrowser
-# from tempfile import NamedTemporaryFile
-#
-# import pynmrstar
-# import requests
 
 import m2mtool.bmrbdep as bmrbdep
 import m2mtool.file_selector as file_selector
-# from m2mtool.configuration import configuration
-# from m2mtool.helpers import ApiSession
-
-#########################
-# Module initialization #
-#########################
 
 # Set up logging
 logging.basicConfig()
-
-
-# def get_vm_version() -> str:
-#     """ Returns the version of the VM that is running."""
-#
-#     try:
-#         with open('/etc/nmrbox.d/motd-identifier', 'r') as motd:
-#             return motd.readline().split(":")[1].strip()
-#     except (IOError, ValueError):
-#         logging.error('Could not determine the version of NMRbox running on this machine.')
-#         return 'unknown'
 
 
 def create_deposition(path) -> None:
@@ -74,45 +52,6 @@ def create_deposition(path) -> None:
 
     # Run the file uploader
     file_selector.run_progress_bar(path, nickname, selected_files, session_file)
-
-    # # Fetch the software list
-    # with ApiSession() as api:
-    #     try:
-    #         url = f"{configuration['api_root_url']}/user/get-bmrbdep-metadata"
-    #         r = api.get(url, json={'path': path, 'vm_id': get_vm_version()})
-    #         r.raise_for_status()
-    #     except requests.exceptions.HTTPError as err:
-    #         logging.exception("Encountered error when retrieving BMRBdep session metadata: %s", err)
-    #         raise IOError('Encountered error when retrieving BMRBdep session metadata.')
-    #
-    #     try:
-    #         with NamedTemporaryFile() as star_file:
-    #             star_file.write(r.text.encode())
-    #             star_file.flush()
-    #             star_file.seek(0)
-    #
-    #             user_email = pynmrstar.Entry.from_string(r.text).get_tag('_Contact_person.Email_address')[0]
-    #
-    #             with bmrbdep.BMRBDepSession(nmrstar_file=star_file,
-    #                                         user_email=user_email,
-    #                                         nickname=nickname) as bmrbdep_session:
-    #
-    #                 Run the progress bar, which handles uploading of data files
-    #                 file_selector.run_progress_bar(bmrbdep_session, selected_files, path)
-    #
-    #                 Delete the metadata file from the "upload file" list
-    #                 bmrbdep_session.delete_file('m2mtool_generated.str')
-    #
-    #             with open(session_file, "w") as session_log:
-    #                 session_info = {"sid": bmrbdep_session.sid, "ctime": time.time()}
-    #                 session_log.write(json.dumps(session_info))
-    #
-    #             # Open the session
-    #             webbrowser.open_new_tab(bmrbdep_session.session_url)
-    #             time.sleep(3)
-    #
-    #     except IOError as err:
-    #         file_selector.show_error(err)
 
 
 # Run the code in this module
